efe90bb13b0139f2d33cc51aa2bff18095ec1d3f
emonsimply <emonsimply@gmail.com>
2025-08-13 12:48:54 +0600

    deleted unnecessary code

--- index.js
+++ index.js
@@ -32,12 +32,7 @@ async function run() {
       res.send(result);
     });
 
-    // app.get("/addTask/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = {_id: new ObjectId(id)}
-    //   const result = await newTaskCollection.findOne(query);
-    //   res.send(result);
-    // })
+
 
     app.post("/addTask", async (req, res) => {
       const newTask = req.body;
